0a48638591145f219b17993345701f81f7fdd581
Jeffrey Dollente <jeffrey.dollente75@gmail.com>
2020-06-03 00:46:02 -0700

    commit attack mode

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,46 @@
-// this creates a function named "fight"
-
 var playerName = window.prompt("What is your robot's name?");
-// What is this?
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10+10);
-//whiat is this?
-console.log("Our robot's name is " + playerName);
- // fight();
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+
+  //Subtract the value of `playerAttack` from the value `enemyAttack` and use that result to update the value in the `enemyHealth` variable
+enemyHealth = enemyHealth - playerAttack;
+
+  // Log a resulting message to the console so we know that it worked.
+console.log(
+    playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+
+// check enemy's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died .");
+}
+else {
+    window.alert(enemyName + " still alive " + enemyHealth + " health left. ");
+}
+  // Subtract the value of `enemyAttack` from the value `playerAttack` and use that result to update the value in the `playerHealth` variable
+playerHealth = playerHealth - enemyAttack;
+  // Log a resulting message to the console so we know that it worked.
+console.log(
+  enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remaining."
+);
+// check player's Health
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died. ");
+}
+else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+
+    }
+}
+
+fight();
