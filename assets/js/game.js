5d47bc5b855714a37baf90d379b7d3da1e765df2
Jeffrey Dollente <jeffrey@Jeffreys-iMac.local>
2020-06-15 21:05:37 -0700

    commit now

--- assets/js/game.js
+++ assets/js/game.js
@@ -29,8 +29,10 @@ var fight = function(enemyName) {
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+// generate random damage value based on player's attack power
+var damage = randomNumber(playerAttack - 3, playerAttack);
+
+enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -40,7 +42,7 @@ var fight = function(enemyName) {
       window.alert(enemyName + ' has died!');
 
       // award player money for winning
-      playerMoney = playerMoney + 20;
+      playerMoney = Math.max(0, playerMoney + 20);
 
       // leave while() loop since enemy is dead
       break;
@@ -48,7 +50,9 @@ var fight = function(enemyName) {
       window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
     }
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -76,7 +80,7 @@ for (var i = 0; i < enemyNames.length; i++) {
 // pick new ememy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
 // reset enemyHealth before starting a new fight
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
 
     fight(pickedEnemyName);
     // if player is still alive and we're not at the last enemy in the array
@@ -117,6 +121,13 @@ if (playAgainConfirm) {
       window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
   };
+
+          // function to generate a random numeric value
+          var randomNumber = function (min, max) {
+            var value = Math.floor(Math.random() * (max - min + 1) + min);
+      
+            return value;
+          };
 
     var shop = function() {
       // ask player what they'd like to do
